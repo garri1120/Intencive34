ffd59e9c4eb1baba1deda28c53a598058fd89a6b
Андрей Попов <123795701+garri1120@users.noreply.github.com>
2023-08-31 12:04:34 +0300

    переименовал константы в StateOfSupportedAutoParts

--- src/main/java/ru/aston/popov_am/task1/Model/StateOfSupportedAutoParts.java
+++ src/main/java/ru/aston/popov_am/task1/Model/StateOfSupportedAutoParts.java
@@ -3,11 +3,11 @@ package ru.aston.popov_am.task1.Model;
 import java.math.BigDecimal;
 
 public enum StateOfSupportedAutoParts {
-    Very_good_condition(new BigDecimal(10)),
-    Good_condition(new BigDecimal(20)),
-    Satisfactory_condition(new BigDecimal(30)),
-    Under_restoration_condition(new BigDecimal(50)),
-    Repair_kit(new BigDecimal(70));
+    Very_GOOD_CONDITION(new BigDecimal(10)),
+    GOOD_CONDITION(new BigDecimal(20)),
+    SATISFACTORY_CONDITION(new BigDecimal(30)),
+    UNDER_RESTORATION_CONDITION(new BigDecimal(50)),
+    REPAIR_KIT(new BigDecimal(70));
     private BigDecimal discountPercentage;
 
     StateOfSupportedAutoParts(BigDecimal discountPercentage) {
